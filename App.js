9773dcec5759e4897fdd2d51317e7d84657232d6
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-04-27 15:16:09 -0500

    Creando nuestra sección de favoritos

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Image} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import CoinsStack from './src/components/coins/CoinsStack';
+import FavoritesStack from './src/components/favorites/FavoritesStack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Colors from './src/res/colors';
 
@@ -23,6 +24,19 @@ const App = () => {
               <Image
                 style={{tintColor: color, width: size, height: size}}
                 source={require('./src/assets/bank.png')}
+              />
+            ),
+          }}
+        />
+
+        <Tabs.Screen
+          name="Favorites"
+          component={FavoritesStack}
+          options={{
+            tabBarIcon: ({size, color}) => (
+              <Image
+                style={{tintColor: color, width: size, height: size}}
+                source={require('./src/assets/star.png')}
               />
             ),
           }}
